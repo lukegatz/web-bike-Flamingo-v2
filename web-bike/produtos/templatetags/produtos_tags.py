89ff809a36af75f75e6d29ab8c207a58590656b6
Luciano <luc.gaubatz@gmail.com>
2020-12-16 23:07:12 -0300

    Formatação do preço na lista e no detalhe do produto
    Criada a template tag 'precificar' para lidar com a formatação dos preços (v. documentação sobre template tags na documentação do django)

---
+++ web-bike/produtos/templatetags/produtos_tags.py
@@ -0,0 +1,18 @@
+from django import template
+import locale
+
+# Register template?
+register = template.Library()
+
+'''
+A template tag precificar aplica o formato de preço ao valor float 
+(no caso, definimos pt_BR como padrão a ser aplicado)
+'''
+@register.filter(name='precificar')
+def precificar(valor):
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+    valor = locale.currency(valor, grouping=True)
+    return valor
+
+
+
